831b211d211e5b13bc97fdd780abceee63787424
QaziAhmad77 <qaziahmad1877@gmail.com>
2024-06-25 12:40:49 +0500

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <div className='flex dark:bg-gray-600 bg-amber-100 lg:text-lg p-4 justify-center gap-6'>
             <Suspense>
-                <NavbarItem title="Trending" param="fetchTrending" />
+                <NavbarItem title="Qazi Ahmad" param="fetchTrending" />
                 <NavbarItem title="Top Rated" param="fetchTopRated" />
             </Suspense>
         </div>
