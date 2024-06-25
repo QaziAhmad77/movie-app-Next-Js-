9bda97d375eb1736eb3116eb439239ffd1056296
QaziAhmad77 <qaziahmad1877@gmail.com>
2024-06-25 12:23:00 +0500

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react'
+import { Suspense } from 'react'
 import NavbarItem from './NavbarItem'
 
 const Navbar = () => {
@@ -6,8 +6,8 @@ const Navbar = () => {
         <div className='flex dark:bg-gray-600 bg-amber-100 lg:text-lg p-4 justify-center gap-6'>
             <Suspense>
                 <NavbarItem title="Trending" param="fetchTrending" />
+                <NavbarItem title="Top Rated" param="fetchTopRated" />
             </Suspense>
-            <NavbarItem title="Top Rated" param="fetchTopRated" />
         </div>
     )
 }
